3597dd7cb605f8ee9549191b7bc132814dac76fc
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-11-24 11:22:39 -0300

    initial router pages (#2)

---
+++ src/Pages/Estoque.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Estoque() {
+  return <h1>Estoque</h1>;
+}
+
+export default Estoque;
---
+++ src/Pages/Licenca.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Licenca() {
+  return <h1>Licenca</h1>;
+}
+
+export default Licenca;
