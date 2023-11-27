d7c43f1e3282cc2e87cb34b6971d15d055ff0de0
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-11-26 23:06:23 -0300

    mobile sets

--- src/components/BannerEntregaImg/index.js
+++ src/components/BannerEntregaImg/index.js
@@ -3,21 +3,50 @@ import ImgBannerEntrega from "../../assets/ImgBanner/BannerEntrega.png";
 import ImgBannerEntregaMobile from "../../assets/ImgBanner/BannerEntregaMobile.png";
 
 export default class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile: window.innerWidth < 800,
+    };
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResize);
+  }
+
+  handleResize() {
+    this.setState({
+      isMobile: window.innerWidth < 800,
+    });
+  }
+
   render() {
-    const isMobile = window.innerWidth <= 400;
+    const { isMobile } = this.state;
 
     return (
-      <div className="container-fluid" style={{ backgroundColor: "#000" }}>
-        {isMobile ? (
-          <img
-            src={ImgBannerEntregaMobile}
-            className="img-fluid"
-            alt="Mobile Banner"
-          />
-        ) : (
-          <img src={ImgBannerEntrega} className="img-fluid" alt="Banner" />
-        )}
-      </div>
+      <>
+        <div className="container-fluid" style={{ backgroundColor: "#000" }}>
+          {isMobile ? (
+            <img
+              src={ImgBannerEntregaMobile}
+              className="img-fluid"
+              alt="Mobile Banner"
+            />
+          ) : (
+            <img
+              src={ImgBannerEntrega}
+              className="img-fluid"
+              alt="Banner"
+              style={{ maxWidth: "120%", maxHeight: "120%" }}
+            />
+          )}
+        </div>
+      </>
     );
   }
 }
--- src/components/BannerPerformanceImg/index.js
+++ src/components/BannerPerformanceImg/index.js
@@ -3,8 +3,30 @@ import ImgBannerPerformance from "../../assets/ImgBanner/BannerPerformance.png";
 import ImgBannerPerformanceMobile from "../../assets/ImgBanner/BannerPerformanceMobile.png";
 
 export default class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile: window.innerWidth < 800,
+    };
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResize);
+  }
+
+  handleResize() {
+    this.setState({
+      isMobile: window.innerWidth < 800,
+    });
+  }
+
   render() {
-    const isMobile = window.innerWidth <= 400;
+    const { isMobile } = this.state;
 
     return (
       <div className="container-fluid" style={{ backgroundColor: "#000" }}>
