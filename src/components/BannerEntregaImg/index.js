4a755ec7352e29d28093827d240d9345f5857f68
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-11-26 23:20:00 -0300

    Refact/carousel mobile img (#25)
    * task complete
    
    * mobile sets
    
    * img desktop ready

--- src/components/BannerEntregaImg/index.js
+++ src/components/BannerEntregaImg/index.js
@@ -3,6 +3,47 @@ import ImgBannerEntrega from "../../assets/ImgBanner/BannerEntrega.png";
 import ImgBannerEntregaMobile from "../../assets/ImgBanner/BannerEntregaMobile.png";
 
 export default class Index extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile: window.innerWidth < 800,
+    };
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResize);
+  }
+
+  handleResize() {
+    this.setState({
+      isMobile: window.innerWidth < 800,
+    });
+  }
+
+  render() {
+    const { isMobile } = this.state;
+
+    return (
+      <>
+        <div className="container-fluid" style={{ backgroundColor: "#000" }}>
+          {isMobile ? (
+            <img
+              src={ImgBannerEntregaMobile}
+              className="img-fluid"
+              alt="Mobile Banner"
+            />
+          ) : (
+            <img src={ImgBannerEntrega} className="img-fluid" alt="Banner" />
+          )}
+        </div>
+      </>
+
   render() {
     const isMobile = window.innerWidth <= 400;
 
@@ -18,6 +59,7 @@ export default class Index extends Component {
           <img src={ImgBannerEntrega} className="img-fluid" alt="Banner" />
         )}
       </div>
+
     );
   }
 }
--- src/components/BannerPerformanceImg/index.js
+++ src/components/BannerPerformanceImg/index.js
@@ -3,8 +3,35 @@ import ImgBannerPerformance from "../../assets/ImgBanner/BannerPerformance.png";
 import ImgBannerPerformanceMobile from "../../assets/ImgBanner/BannerPerformanceMobile.png";
 
 export default class Index extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile: window.innerWidth < 800,
+    };
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResize);
+  }
+
+  handleResize() {
+    this.setState({
+      isMobile: window.innerWidth < 800,
+    });
+  }
+
+  render() {
+    const { isMobile } = this.state;
+
   render() {
     const isMobile = window.innerWidth <= 400;
+
 
     return (
       <div className="container-fluid" style={{ backgroundColor: "#000" }}>
