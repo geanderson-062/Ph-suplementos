68f3a5cec7227dbfd8594584e0e6ebb46e1de9a3
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-11-30 20:45:14 -0300

    new routes api

--- src/service/MaisVendido.js
+++ src/service/MaisVendido.js
@@ -1,9 +1,19 @@
 const ProducsData = [
   {
-    title: "Creatina Integral medica",
-    description: "150g",
-    priceWithoutDiscount: "90.00$",
-    discountPrice: "85,99$",
+    title: "Whey Protein Max Titanium",
+    description: "900g",
+    priceWithoutDiscount: "150.00",
+    discountPrice: "145,99",
+    imageUrl:
+      "https://d2vq4s943o8cb4.cloudfront.net/Custom/Content/Products/02/76/0276_creatina-hardcore-integralmedica-2312_m2_636668372702493590.png",
+    link: "/",
+  },
+
+  {
+    title: "Whey Protein Integral medica",
+    description: "900g Refil",
+    priceWithoutDiscount: "100.00",
+    discountPrice: "95,99",
     imageUrl:
       "https://d2vq4s943o8cb4.cloudfront.net/Custom/Content/Products/02/76/0276_creatina-hardcore-integralmedica-2312_m2_636668372702493590.png",
     link: "/",
@@ -12,26 +22,6 @@ const ProducsData = [
   {
     title: "Creatina Max Titanium",
     description: "100g",
-    priceWithoutDiscount: "90.00$",
-    discountPrice: "85,99$",
-    imageUrl:
-      "https://d2vq4s943o8cb4.cloudfront.net/Custom/Content/Products/02/76/0276_creatina-hardcore-integralmedica-2312_m2_636668372702493590.png",
-    link: "/",
-  },
-
-  {
-    title: "Creatina Integral medica",
-    description: "300g",
-    priceWithoutDiscount: "90.00$",
-    discountPrice: "85,99$",
-    imageUrl:
-      "https://d2vq4s943o8cb4.cloudfront.net/Custom/Content/Products/02/76/0276_creatina-hardcore-integralmedica-2312_m2_636668372702493590.png",
-    link: "/",
-  },
-
-  {
-    title: "Creatina Max Titanium",
-    description: "300g",
     priceWithoutDiscount: "90.00$",
     discountPrice: "85,99$",
     imageUrl:
