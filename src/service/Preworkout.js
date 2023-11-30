0c593b318af6599f582d444718e1bfb489a9b4ae
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-11-30 19:32:22 -0300

    separate products for categories

---
+++ src/service/Preworkout.js
@@ -0,0 +1,53 @@
+const ProducsData = [
+  {
+    title: "Pré treino BOPE Black-Skull",
+    description: "150g",
+    priceWithoutDiscount: "100.00$",
+    discountPrice: "95,99$",
+    imageUrl:
+      "https://d2vq4s943o8cb4.cloudfront.net/Custom/Content/Products/02/76/0276_creatina-hardcore-integralmedica-2312_m2_636668372702493590.png",
+    link: "/",
+  },
+
+  {
+    title: "Pré treino Crack Demons-Lab",
+    description: "300g",
+    priceWithoutDiscount: "100.00$",
+    discountPrice: "95,99$",
+    imageUrl:
+      "https://d2vq4s943o8cb4.cloudfront.net/Custom/Content/Products/02/76/0276_creatina-hardcore-integralmedica-2312_m2_636668372702493590.png",
+    link: "/",
+  },
+
+  {
+    title: "Pré treino Crack Diabo Verde",
+    description: "300g",
+    priceWithoutDiscount: "100.00$",
+    discountPrice: "95,99$",
+    imageUrl:
+      "https://d2vq4s943o8cb4.cloudfront.net/Custom/Content/Products/02/76/0276_creatina-hardcore-integralmedica-2312_m2_636668372702493590.png",
+    link: "/",
+  },
+
+  {
+    title: "Pré treino Psicho Killer Demons-Lab",
+    description: "294g",
+    priceWithoutDiscount: "100.00$",
+    discountPrice: "95,99$",
+    imageUrl:
+      "https://d2vq4s943o8cb4.cloudfront.net/Custom/Content/Products/02/76/0276_creatina-hardcore-integralmedica-2312_m2_636668372702493590.png",
+    link: "/",
+  },
+
+  {
+    title: "Pré treino Égide Max titanium",
+    description: "150g",
+    priceWithoutDiscount: "100.00$",
+    discountPrice: "95,99$",
+    imageUrl:
+      "https://d2vq4s943o8cb4.cloudfront.net/Custom/Content/Products/02/76/0276_creatina-hardcore-integralmedica-2312_m2_636668372702493590.png",
+    link: "/",
+  },
+];
+
+export default ProducsData;
---
+++ src/service/Whey.js
@@ -0,0 +1,53 @@
+const ProducsData = [
+  {
+    title: "Whey Protein Max Titanium",
+    description: "900g",
+    priceWithoutDiscount: "150.00$",
+    discountPrice: "145,99$",
+    imageUrl:
+      "https://d2vq4s943o8cb4.cloudfront.net/Custom/Content/Products/02/76/0276_creatina-hardcore-integralmedica-2312_m2_636668372702493590.png",
+    link: "/",
+  },
+
+  {
+    title: "Whey Protein Integral medica",
+    description: "900g Refil",
+    priceWithoutDiscount: "100.00$",
+    discountPrice: "95,99$",
+    imageUrl:
+      "https://d2vq4s943o8cb4.cloudfront.net/Custom/Content/Products/02/76/0276_creatina-hardcore-integralmedica-2312_m2_636668372702493590.png",
+    link: "/",
+  },
+
+  {
+    title: "Iso blend Synthe-Size",
+    description: "907g",
+    priceWithoutDiscount: "100.00$",
+    discountPrice: "95,99$",
+    imageUrl:
+      "https://d2vq4s943o8cb4.cloudfront.net/Custom/Content/Products/02/76/0276_creatina-hardcore-integralmedica-2312_m2_636668372702493590.png",
+    link: "/",
+  },
+
+  {
+    title: "Whey Protein Black-Skull",
+    description: "900g",
+    priceWithoutDiscount: "100.00$",
+    discountPrice: "95,99$",
+    imageUrl:
+      "https://d2vq4s943o8cb4.cloudfront.net/Custom/Content/Products/02/76/0276_creatina-hardcore-integralmedica-2312_m2_636668372702493590.png",
+    link: "/",
+  },
+
+  {
+    title: "Isolate Protein Profit",
+    description: "900g",
+    priceWithoutDiscount: "100.00$",
+    discountPrice: "95,99$",
+    imageUrl:
+      "https://d2vq4s943o8cb4.cloudfront.net/Custom/Content/Products/02/76/0276_creatina-hardcore-integralmedica-2312_m2_636668372702493590.png",
+    link: "/",
+  },
+];
+
+export default ProducsData;
