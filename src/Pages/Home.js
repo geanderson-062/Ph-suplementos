6bcf30424f00587cf26544eb3acc294578969304
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-11-30 20:58:14 -0300

    home cards products

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,13 +2,29 @@
 import React from "react";
 //components
 import Header from "../components/Header";
+//titles section
 import TitleHome from "../components/TItles/PageTitleHome";
+import TitlePromocao from "../components/TItles/PageTitlePromocao";
+import TitleMaisVendido from "../components/TItles/PageTitleMaisVendido";
+import TitleDestaques from "../components/TItles/PageTitleDestaques";
+//cards products
+import CardPromocao from "../components/Card/PromocaoCard";
+import CardDestaques from "../components/Card/DestaqueCard";
+import CardMaisVendido from "../components/Card/MaisVendidoCard";
 
 function Home() {
   return (
     <>
       <Header />
       <TitleHome />
+      <section className="container">
+        <TitlePromocao />
+        <CardPromocao />
+        <TitleMaisVendido />
+        <CardMaisVendido />
+        <TitleDestaques />
+        <CardDestaques />
+      </section>
     </>
   );
 }
