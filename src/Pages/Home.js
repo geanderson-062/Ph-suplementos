bdf776aaab12f3993471190f90d936dea1aa65e6
Wedgles97 <wedegles@gmail.com>
2023-12-02 16:55:52 -0300

    chore: created component footer

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,7 @@ import React from "react";
 //components
 import Header from "../components/Header";
 import ProductCard from "../components/Card/ProductCard";
+import Footer from "../components/Footer";
 //titles section
 import TitlePromocao from "../components/TItles/PageTitlePromocao";
 import TitleMaisVendido from "../components/TItles/PageTitleMaisVendido";
@@ -80,6 +81,7 @@ function Home() {
         </div>
         <ButtonDestaque />
       </section>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+
+export default function index() {
+  return (
+    <div className="bg-dark text-white">
+      <div className="container py-4">
+        <div className="row">
+          <div className="col-3">
+            <h4 className="h6">Páginas</h4>
+            <ul className="list-unstyled">
+              <li>
+                <a href="http://">Produtos</a>
+              </li>
+              <li>
+                <a href="http://">Termo e condições</a>
+              </li>
+
+              <li>
+                <a href="http://">Forma de Pagamento</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-3">
+            <h4 className="h6">Páginas</h4>
+            <ul className="list-unstyled">
+              <li>
+                <a href="http://">Destaques</a>
+              </li>
+              <li>
+                <a href="http://">Contato</a>
+              </li>
+
+              <li>
+                <a href="http://">Nossa Loja</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-4">
+            <h4 className="h6 ">Formas de Pagamento</h4>
+            <div className="container">
+              <div className="row flex-column">
+                <div className="col-md-4 mb-3">
+                  <ul className="list-unstyled d-flex justify-content-between">
+                    <li>
+                      <img
+                        src="./assets/ImgFooter/iconFooter/Mastercard.svg"
+                        alt="Mastercard"
+                      />
+                    </li>
+                    <li>
+                      <img
+                        src="./assets/ImgFooter/iconFooter/Visa.svg"
+                        alt="Visa"
+                      />
+                    </li>
+                    <li>
+                      <img
+                        src="./assets/ImgFooter/iconFooter/Hypercard.svg"
+                        alt="Hypercard"
+                      />
+                    </li>
+                    <li>
+                      <img
+                        src="./assets/ImgFooter/iconFooter/Boleto.svg"
+                        alt="Boleto"
+                      />
+                    </li>
+                  </ul>
+                </div>
+                <div className="col-md-4 mb-3 mt-md-n2">
+                  <ul className="list-unstyled d-flex justify-content-between">
+                    <li>
+                      <img
+                        src="./assets/ImgFooter/iconFooter/MercadoPago.svg"
+                        alt="Mercado Pago"
+                      />
+                    </li>
+                    <li>
+                      <img
+                        src="./assets/ImgFooter/iconFooter/PayPal.svg"
+                        alt="PayPal"
+                      />
+                    </li>
+                    <li>
+                      <img
+                        src="./assets/ImgFooter/iconFooter/Elo.svg"
+                        alt="Elo"
+                      />
+                    </li>
+                    <li>
+                      <img
+                        src="./assets/ImgFooter/iconFooter/Pix.svg"
+                        alt="Pix"
+                      />
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
