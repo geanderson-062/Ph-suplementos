e698dddad7a11f67011b55b65b183a9c74516f41
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-12-02 10:19:40 -0300

    preparing to clean card components

--- src/Pages/Hypercaloric.js
+++ src/Pages/Hypercaloric.js
@@ -2,7 +2,10 @@ import React from "react";
 //components
 import Header from "../components/Header";
 import TitleHypercaloric from "../components/TItles/PageTitleHypercaloric";
-import Card from "../components/Card/HypercaloricCard";
+import ProductCard from "../components/Card/ProductCard";
+//services
+import ProducsData from "../service/Products/Hypercaloric";
+import MensageBuyService from "../service/MensageBuyService/MensageBuyService";
 
 function Hypercaloric() {
   return (
@@ -10,7 +13,23 @@ function Hypercaloric() {
       <Header />
       <TitleHypercaloric />
       <section className="container">
-        <Card />
+        <div
+          className="row row-cols-1 row-cols-md-4 g-4"
+          style={{ marginTop: 50, marginLeft: "5%", marginRight: "5%" }}
+        >
+          {ProducsData.map((product, index) => {
+            const Contato = MensageBuyService(product);
+
+            return (
+              <ProductCard
+                key={index}
+                product={product}
+                index={index}
+                Contato={Contato}
+              />
+            );
+          })}
+        </div>
       </section>
     </>
   );
--- src/Pages/MaisVendido.js
+++ src/Pages/MaisVendido.js
@@ -2,7 +2,10 @@ import React from "react";
 //components
 import Header from "../components/Header";
 import TitleMaisVendido from "../components/TItles/PageTitleMaisVendido";
-import Card from "../components/Card/MaisVendidoCard";
+import ProductCard from "../components/Card/ProductCard";
+//services
+import ProducsData from "../service/SeparatePage/MaisVendido";
+import MensageBuyService from "../service/MensageBuyService/MensageBuyService";
 
 function Preworkout() {
   return (
@@ -10,7 +13,23 @@ function Preworkout() {
       <Header />
       <TitleMaisVendido />
       <section className="container">
-        <Card />
+        <div
+          className="row row-cols-1 row-cols-md-4 g-4"
+          style={{ marginTop: 50, marginLeft: "5%", marginRight: "5%" }}
+        >
+          {ProducsData.map((product, index) => {
+            const Contato = MensageBuyService(product);
+
+            return (
+              <ProductCard
+                key={index}
+                product={product}
+                index={index}
+                Contato={Contato}
+              />
+            );
+          })}
+        </div>
       </section>
     </>
   );
