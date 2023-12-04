fdd14d8c92a154437f4f4f0cfb790415086675a6
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-12-04 10:00:29 -0300

    animation in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,25 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
+import ScrollReveal from "scrollreveal";
 import Navbar from "../Navbar";
 import Carousel from "../Carousel";
-export default class index extends Component {
-  render() {
-    return (
-      <>
-        <header>
-          <Navbar />
-          <Carousel />
-        </header>
-      </>
-    );
-  }
-}
+
+const Header = () => {
+  useEffect(() => {
+    ScrollReveal().reveal(".header", {
+      duration: 1000,
+      origin: "top",
+      distance: "20px",
+      easing: "ease-in-out",
+    });
+  }, []);
+  return (
+    <>
+      <header className="header">
+        <Navbar />
+        <Carousel />
+      </header>
+    </>
+  );
+};
+
+export default Header;
